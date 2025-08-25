d8658b67e16083f72d1c2960634845e38785490d
Audreylyn Moraña <128884392+audreylyn@users.noreply.github.com>
2025-08-25 20:28:07 +0800

    Implement environment-based database credentials

--- auth/dbh.inc.php
+++ auth/dbh.inc.php
@@ -1,14 +1,27 @@
 <?php
 function db() {
+    // Database settings
     $host = "mcismartdb.mysql.database.azure.com";
-    $username = "adminuser@mcismartdb";  // must include @servername
-    $password = "SmartDb2025!";
     $database = "mcismartdb";
+    
+    // Detect environment
+    if (strpos($_SERVER['HTTP_HOST'], 'azurewebsites.net') !== false) {
+        // Running on Azure App Service
+        $username = "adminuser@mcismartdb";   // Must include @servername
+        $password = "SmartDb2025!";
+    } else {
+        // Running locally (e.g., XAMPP + MySQL Workbench)
+        $username = "adminuser";              // Just the username
+        $password = "";
+    }
 
+    // Create connection
     $conn = new mysqli($host, $username, $password, $database);
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     return $conn;
 }
+$conn = db();
 ?>
